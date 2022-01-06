96f7b276d2d35c7a2c168b69d255d60511cb6d9c
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-01-06 20:36:51 +0100

    Redirect standard output to the blackhole

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -6,7 +6,7 @@ echo "\n<<< Starting ZSH setup >>>\n"
 
 # Change default shell to the one installed by brew (instead of the one in /bin/zsh)
 echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells'
+echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
 echo "Enter user password to change login shell"
 chsh -s '/opt/homebrew/bin/zsh'
