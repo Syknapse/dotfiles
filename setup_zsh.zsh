2eec5b9647c09f5086a13caa10e0ea63a0c6dac0
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-01-07 10:18:54 +0100

    Add conditionals to prevent zsh setup from repeating commands

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -5,8 +5,18 @@ echo "\n<<< Starting ZSH setup >>>\n"
 # We do not need to install zsh here because it is in the brewfile and will be installed
 
 # Change default shell to the one installed by brew (instead of the one in /bin/zsh)
-echo "Enter superuser (sudo) password to edit /etc/shells"
-echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
 
-echo "Enter user password to change login shell"
-chsh -s '/opt/homebrew/bin/zsh'
+if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+  echo 'Already added Homebrew ZSH to acceptable shells: /opt/homebrew/bin/zsh exists in /etc/shells'
+else
+  echo "Enter superuser (sudo) password to edit /etc/shells"
+  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+fi
+
+if [ "$SHELL" = '/opt/homebrew/bin/zsh' ]; then
+  echo 'Already using Hombrew ZSH as default user shell: $SHELL is /opt/homebrew/bin/zsh'
+else
+  echo "Enter user password to change login shell"
+  chsh -s '/opt/homebrew/bin/zsh'
+fi
+
